3fb16c764c4d422a4d981a6f1784bb91812fefec
Musab Nazir <musabnazir@gmail.com>
2025-08-06 13:31:04 -0400

    interpretor finished

--- src/brain.nim
+++ src/brain.nim
@@ -1,9 +1,48 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import os
 
 when isMainModule:
-  var x = 0
-  for i in 1 .. 100_000_000:
-    x += 1
+  # import the code from the input file
+  let code = if paramCount() > 0: readFile paramStr(1)
+             else: readAll stdin
 
-  echo("Hello, World! ", x)
+  # echo code
+
+  var
+    tape = newSeq[char]()
+    tapePos = 0
+    codePos = 0
+
+  {.push overflowchecks: off.}
+  proc xinc*(c: var char) {.inline.} =
+    ## Increment a character with wrapping instead of overflow checks.
+    inc c
+  proc xdec*(c: var char) {.inline.} =
+    ## Decrement a character with wrapping instead of underflow checks.
+    dec c
+  {.pop.}
+
+  proc run(skip = false): bool =
+    while tapePos >= 0 and codePos < code.len:
+      if tapePos >= tape.len:
+        tape.add '\0'
+
+      if code[codePos] == '[':
+        inc codePos
+        let oldPos = codePos
+        while run(tape[tapePos] == '\0'):
+          codePos = oldPos
+      elif code[codePos] == ']':
+        return tape[tapePos] != '\0'
+      elif not skip:
+        case code[codePos]
+        of '+': xinc tape[tapePos]
+        of '-': xdec tape[tapePos]
+        of '>': inc tapePos
+        of '<': dec tapePos
+        of '.': stdout.write tape[tapePos]
+        of ',': tape[tapePos] = stdin.readChar
+        else: discard
+
+      inc codePos
+
+  discard run()
